06d3b1326d02372a22c0d54aea017cf775a89927
Nate Hunter <hunter.j.nate@gmail.com>
2021-10-14 22:04:29 -0400

    html elements added + some styling

---
+++ src/quotes.js
@@ -0,0 +1,23 @@
+
+// RANDOM SEINFELD QUOTE;
+function getRandomSeinfeldQuote() {
+    fetch("https://seinfeld-quotes.herokuapp.com/random")
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+}
+
+
+// ZEN QUOTES:
+function getRandomZenQuote() {
+    const url = 'https://zenquotes.io/api/random/'
+    fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {}
+    })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+}
+
+getRandomSeinfeldQuote();
+// getRandomZenQuote();
